bb2619357b4942cca0a26f81573ce1ff9e486b5a
dl3c01n <35270127+dl3c01n@users.noreply.github.com>
2018-07-03 11:17:41 +0200

    Loading footer component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" style="background-color: #a8a8a8">
     <app-key></app-key>
+    <app-foot style="width: 100%"></app-foot>
   </div>
 </template>
 
 <script>
 import Keyword from "./components/Home.vue"
+import Foot from "./components/Foot.vue"
 
 export default {
   name: 'app',
   components:{
-    appKey: Keyword
+    appKey: Keyword,
+    appFoot: Foot
   }
 }
 </script>
